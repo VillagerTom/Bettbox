08641116cc848356f4914d34a1c76b786aa2d93c
VillagerTom <villager-tom@proton.me>
2026-05-04 05:58:30 +0800

    chore(build): display arch names when building core of multiple archs

--- setup.dart
+++ setup.dart
@@ -289,7 +289,7 @@ class Build {
     final items = buildItems.where((element) {
       return element.platform == platform && (arch == null || element.arch == arch);
     }).toList();
-
+    final isMultiArch = items.length > 1;
     final List<String> corePaths = [];
     final List<String> macOSCorePaths = [];
     for (final item in items) {
@@ -372,7 +372,7 @@ class Build {
       ];
       await exec(
         execLines,
-        name: 'build core',
+        name: 'build core ${isMultiArch ? "(${item.archName ?? item.arch.name})" : ""}',
         environment: env,
         workingDirectory: _coreDir,
       );
